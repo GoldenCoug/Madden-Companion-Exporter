f2c1624a0a17f536194b09e00dda161c7b41a940
GoldenCoug <70071621+GoldenCoug@users.noreply.github.com>
2020-08-22 11:08:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
 // const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
-
+./ madden-21-monstars-firebase
 // TODO: Enter your database url from firebase
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 //   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+// });https://madden-21-monstars.firebaseio.com/
 
 app.set('port', (process.env.PORT || 3001));
 
